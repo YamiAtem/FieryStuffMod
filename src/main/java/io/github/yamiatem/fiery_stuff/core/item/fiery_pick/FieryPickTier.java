7c3a76200e2b66c383d47eeb8abec5c72ec9fe8a
Atharva Mishra <atharvamishra956@gmail.com>
2021-02-13 21:05:55 +0530

    made FieryPickTier cleaner

--- src/main/java/io/github/yamiatem/fiery_stuff/core/item/fiery_pick/FieryPickTier.java
+++ src/main/java/io/github/yamiatem/fiery_stuff/core/item/fiery_pick/FieryPickTier.java
@@ -6,10 +6,33 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.Ingredient;
 
 public class FieryPickTier implements IItemTier {
-	 @Override public int   getMaxUses()             { return 1000; }
-	 @Override public float getEfficiency()          { return 10; }
-	 @Override public float getAttackDamage()        { return 10; }
-	 @Override public int   getHarvestLevel()        { return 3; }
-	 @Override public int   getEnchantability()      { return 5; }
-	 @Override public Ingredient getRepairMaterial() { return Ingredient.fromStacks(new ItemStack(FieryIngotInit.FIERY_INGOT.get())); }
+	@Override
+	public int getMaxUses() {
+		return 1000;
+	}
+
+	@Override
+	public float getEfficiency() {
+		return 10;
+	}
+
+	@Override
+	public float getAttackDamage() {
+		return 10;
+	}
+
+	@Override
+	public int getHarvestLevel() {
+		return 3;
+	}
+
+	@Override
+	public int getEnchantability() {
+		return 5;
+	}
+
+	@Override
+	public Ingredient getRepairMaterial() {
+		return Ingredient.fromStacks(new ItemStack(FieryIngotInit.FIERY_INGOT.get()));
+	}
 }
